e1ab536152e7e7c2e90dfd8165bc82790a8a71b3
gitperalta <sebssjb@gmail.com>
2023-02-13 17:31:22 -0500

    deploy

--- api/src/app.js
+++ api/src/app.js
@@ -18,7 +18,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://pi-videogames-sebastianperalta.vercel.app/home"
+    "https://pi-videogames-sebastianperalta.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
