73d248d7dc997cc420bcd0b73c72d376603d5361
gitperalta <sebssjb@gmail.com>
2023-02-13 18:49:13 -0500

    deploy

--- api/index.js
+++ api/index.js
@@ -23,7 +23,8 @@ require("dotenv").config();
 const { PORT } = process.env;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log("%s listening at ", PORT); // eslint-disable-line no-console
-  });
+  server.listen(
+    PORT,
+    () => console.log(`%s listening at ${PORT}`) // eslint-disable-line no-console
+  );
 });
